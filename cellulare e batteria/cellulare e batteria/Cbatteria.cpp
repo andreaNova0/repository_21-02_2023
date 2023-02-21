ae58506e3c8d9f739a97470b8fad83ddf6ed8672
Romeo-Colleghi <125869374+Romeo-Colleghi@users.noreply.github.com>
2023-02-21 11:27:50 +0100

    update

--- cellulare e batteria/cellulare e batteria/Cbatteria.cpp
+++ cellulare e batteria/cellulare e batteria/Cbatteria.cpp
@@ -1,1 +1,17 @@
 #include "Cbatteria.h"
+
+Cbatteria::Cbatteria()
+{
+}
+
+Cbatteria::Cbatteria(int capacitàcarica, int livelloCarica)
+{
+	//int setCapCarica(capacitàcarica);
+	this->capacitàcarica = capacitàcarica;
+	this->livelloCarica = livelloCarica;
+}
+
+int Cbatteria::getBatteria()
+{
+	return 0;
+}
